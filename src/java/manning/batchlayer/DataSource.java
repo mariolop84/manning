ba641c62c939e61cffdf698c5fcdc6dc1e03be17
Mario <mariolop84@gmail.com>
2016-01-21 10:56:22 +0100

    Creacion del fichero build.xml Creacion del fichero build para la compilacion y ejecucion

--- src/java/manning/batchlayer/DataSource.java
+++ src/java/manning/batchlayer/DataSource.java
@@ -1,3 +1,4 @@
+package java.manning.batchlayer;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.conf.Configuration;
 
@@ -8,19 +9,6 @@ public class DataSource {
     public static final String MASTER_ROOT = DATA_ROOT + "master";
     public static final String NEW_ROOT = DATA_ROOT + "new";
 
-    public static void initTestData() throws Exception {
-        FileSystem fs = FileSystem.get(new Configuration());
-        fs.delete(new Path(DATA_ROOT), true);
-        fs.delete(new Path(OUTPUTS_ROOT), true);
-        fs.mkdirs(new Path(DATA_ROOT));
-        fs.mkdirs(new Path(OUTPUTS_ROOT + "edb"));
-
-public class BatchWorkflow {
-    public static final String ROOT = "/tmp/swaroot/";
-    public static final String DATA_ROOT = ROOT + "data/";
-    public static final String OUTPUTS_ROOT = ROOT + "outputs/";
-    public static final String MASTER_ROOT = DATA_ROOT + "master";
-    public static final String NEW_ROOT = DATA_ROOT + "new";
 
     public static void initTestData() throws Exception {
         FileSystem fs = FileSystem.get(new Configuration());
