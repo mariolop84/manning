e3ff8b69468fdc169f4700b99205f58b5ddba824
Mario <mariolop84@gmail.com>
2016-02-17 09:10:23 +0100

    Update Principal.java

--- src/es/manning/batchlayer/Principal.java
+++ src/es/manning/batchlayer/Principal.java
@@ -2,21 +2,16 @@ package es.manning.batchlayer;
 
 import backtype.hadoop.pail.Pail;
 import es.manning.batchlayer.DataSource;
+import es.manning.batchlayer.Constants;
 
 public class Principal {
 
-	public static final String ROOT = "/tmp/swaroot/";
-	public static final String DATA_ROOT = ROOT + "data/";
-	public static final String OUTPUTS_ROOT = ROOT + "outputs/";
-	public static final String MASTER_ROOT = DATA_ROOT + "master";
-	public static final String NEW_ROOT = DATA_ROOT + "new";
-	
 	public static void main(String[] args) {
 
 		try {
 		
-			Pail masterPail = new Pail(MASTER_ROOT);
-			Pail newDataPail = new Pail(NEW_ROOT);
+			Pail masterPail = new Pail(Constants.MASTER_ROOT);
+			Pail newDataPail = new Pail(Constants.NEW_ROOT);
 			Ingestor ing = new Ingestor();
 			
 			ing.ingest(masterPail, newDataPail);
