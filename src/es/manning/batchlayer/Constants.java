3db64018aad1faa62b27e94581fd08329f8e3bdd
Mario <mariolop84@gmail.com>
2016-02-17 09:09:12 +0100

    Clase con las constantes globales

---
+++ src/es/manning/batchlayer/Constants.java
@@ -0,0 +1,11 @@
+package es.manning.batchlayer;
+
+public class Constants {
+
+  public static final String ROOT = "/tmp/swaroot/";
+	public static final String DATA_ROOT = ROOT + "data/";
+	public static final String OUTPUTS_ROOT = ROOT + "outputs/";
+	public static final String MASTER_ROOT = DATA_ROOT + "master";
+	public static final String NEW_ROOT = DATA_ROOT + "new";
+	
+}
