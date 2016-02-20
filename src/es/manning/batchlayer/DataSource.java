b874c6aa138005ea1cd296c33481c67aaaaef3ef
Mario Lopez <mariolop84@gmail.com>
2016-02-20 19:34:00 +0100

    void

--- src/es/manning/batchlayer/DataSource.java
+++ src/es/manning/batchlayer/DataSource.java
@@ -6,6 +6,8 @@ import java.io.IOException;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
+import org.apache.log4j.Logger;
+
 import backtype.hadoop.pail.Pail;
 import backtype.hadoop.pail.Pail.TypedRecordOutputStream;
 import es.manning.tap.SplitDataPailStructure;
@@ -13,59 +15,69 @@ import es.manning.test.Data;
 import es.manning.schema.*;
 
 public class DataSource {
-	public static final String ROOT = "/tmp/swaroot/";
-	public static final String DATA_ROOT = ROOT + "data/";
-	public static final String OUTPUTS_ROOT = ROOT + "outputs/";
-	public static final String MASTER_ROOT = DATA_ROOT + "master";
-	public static final String NEW_ROOT = DATA_ROOT + "new";
+	static Logger logger = Logger.getLogger(DataSource.class);
+	
+
 
 	@SuppressWarnings({ "unchecked", "rawtypes" })
 	public static void initTestData() throws Exception {
+		logger.info("Principal.initTestData: INICIO");
 		FileSystem fs = FileSystem.get(new Configuration());
-		fs.delete(new Path(DATA_ROOT), true);
-		fs.delete(new Path(OUTPUTS_ROOT), true);
-		fs.mkdirs(new Path(DATA_ROOT));
-		fs.mkdirs(new Path(OUTPUTS_ROOT + "edb"));
+		fs.delete(new Path(Constants.DATA_ROOT), true);
+		fs.delete(new Path(Constants.OUTPUTS_ROOT), true);
+		fs.mkdirs(new Path(Constants.DATA_ROOT));
+		fs.mkdirs(new Path(Constants.OUTPUTS_ROOT + "edb"));
 
-		//Pail masterPail = Pail.create(MASTER_ROOT, new SplitDataPailStructure());
-		
-		//Pail<Data> newPail = Pail.create(NEW_ROOT, new DataPailStructure());
-		Pail<es.manning.schema.Data> newPail = Pail.create(NEW_ROOT, new SplitDataPailStructure());
+		Pail<es.manning.schema.Data> masterPail = Pail.create(Constants.MASTER_ROOT, new SplitDataPailStructure());
+		Pail<es.manning.schema.Data> newPail = Pail.create(Constants.NEW_ROOT, new SplitDataPailStructure());
 
-		TypedRecordOutputStream os = newPail.openWrite();
-		
-		os.writeObject(Data.makePageview(1, "http://foo.com/post1", 60));
-		os.writeObject(Data.makePageview(3, "http://foo.com/post1", 62));
-		os.writeObject(Data.makePageview(1, "http://foo.com/post1", 4000));
-		os.writeObject(Data.makePageview(1, "http://foo.com/post2", 4000));
-		os.writeObject(Data.makePageview(1, "http://foo.com/post2", 10000));
-		os.writeObject(Data.makePageview(5, "http://foo.com/post3", 10600));
-		os.writeObject(Data.makeEquiv(1, 3));
-		os.writeObject(Data.makeEquiv(3, 5));
+		TypedRecordOutputStream os = masterPail.openWrite();
 
-		os.writeObject(Data.makePageview(2, "http://foo.com/post1", 60));
-		os.writeObject(Data.makePageview(2, "http://foo.com/post3", 62));
-	        os.writeObject(Data.makePersonPropertyValueFull_name(1, "Pepito"));
-	        os.writeObject(Data.makePersonPropertyValueFull_name(2, "Pepita"));
-        	os.writeObject(Data.makePersonPropertyValueGender(1, GenderType.MALE));
-	        os.writeObject(Data.makePersonPropertyValueGender(2, GenderType.FEMALE));
-        	os.writeObject(Data.makePersonPropertyValueLocation(1, "Miami", "FL", "USA"));
-	        os.writeObject(Data.makePersonPropertyValueLocation(2,  "LA", "California", "USA"));
+		os.writeObject(es.manning.test.Data.makePageview(1, "http://foo.com/post1", 60));
+		os.writeObject(es.manning.test.Data.makePageview(3, "http://foo.com/post1", 62));
+		os.writeObject(es.manning.test.Data.makePageview(1, "http://foo.com/post1", 4000));
+		os.writeObject(es.manning.test.Data.makePageview(1, "http://foo.com/post2", 4000));
+		os.writeObject(es.manning.test.Data.makePageview(1, "http://foo.com/post2", 10000));
+		os.writeObject(es.manning.test.Data.makePageview(5, "http://foo.com/post3", 10600));
+		os.writeObject(es.manning.test.Data.makeEquiv(1, 3));
+		os.writeObject(es.manning.test.Data.makeEquiv(3, 5));
+
+		os.writeObject(es.manning.test.Data.makePageview(2, "http://foo.com/post1", 60));
+		os.writeObject(es.manning.test.Data.makePageview(2, "http://foo.com/post3", 62));
+		os.writeObject(es.manning.test.Data.makePersonPropertyValueFull_name(1, "Pepito"));
+		os.writeObject(es.manning.test.Data.makePersonPropertyValueFull_name(2, "Pepita"));
+		os.writeObject(es.manning.test.Data.makePersonPropertyValueGender(1, GenderType.MALE));
+		os.writeObject(es.manning.test.Data.makePersonPropertyValueGender(2, GenderType.FEMALE));
+		os.writeObject(es.manning.test.Data.makePersonPropertyValueLocation(1, "Miami", "FL", "USA"));
+		os.writeObject(es.manning.test.Data.makePersonPropertyValueLocation(2, "LA", "California", "USA"));
 		os.close();
-
+		
+		os = newPail.openWrite();		
+		os.writeObject(es.manning.test.Data.makePageview(7, "http://foo.com/post3", 60));
+		os.writeObject(es.manning.test.Data.makePageview(7, "http://foo.com/post1", 62));
+		os.writeObject(es.manning.test.Data.makePageview(9, "http://foo.com/post1", 60));
+		os.writeObject(es.manning.test.Data.makePageview(9, "http://foo.com/post2", 4000));
+		os.writeObject(es.manning.test.Data.makePersonPropertyValueFull_name(7, "Cata"));
+		os.writeObject(es.manning.test.Data.makePersonPropertyValueGender(7, GenderType.FEMALE));
+		os.writeObject(es.manning.test.Data.makePersonPropertyValueLocation(7, "Madrid", "MAD", "Spain"));
+		os.writeObject(es.manning.test.Data.makePersonPropertyValueFull_name(9, "Mario"));
+		os.writeObject(es.manning.test.Data.makePersonPropertyValueGender(9, GenderType.FEMALE));
+		os.writeObject(es.manning.test.Data.makePersonPropertyValueLocation(9, "Madrid", "MAD", "Spain"));
+		os.close();
+		logger.info("Principal.initTestData: FIN");
 	}
 
 	
 	
 	
 	//test to read
-	public static void readLogins() throws IOException {
-		System.out.println("DataSource.readLogins: INICIO");
-		Pail<es.manning.schema.Data> dataPail = new Pail<es.manning.schema.Data>(NEW_ROOT);
+	public static void readData() throws IOException {
+		logger.info("DataSource.readLogins: INICIO");
+		Pail<es.manning.schema.Data> dataPail = new Pail<es.manning.schema.Data>(Constants.MASTER_ROOT);
 		for (es.manning.schema.Data d : dataPail) {
-			System.out.println(d.toString());
+			logger.info(d.toString());
 		}
-		System.out.println("DataSource.readLogins: FIN");
+		logger.info("DataSource.readLogins: FIN");
 	}
 
 }
--- src/es/manning/batchlayer/Principal.java
+++ src/es/manning/batchlayer/Principal.java
@@ -28,10 +28,12 @@ public class Principal {
 		try {
 			switch (entrada) {
 			case "initTestData":
-				Principal.initTestData();
+				DataSource.initTestData();
+				DataSource.readData();
 				break;
 			case "ingest":
 				Principal.ingest();
+				DataSource.readData();
 				break;
 			default:
 				logger.info("Principal.main.Error: Entrada no valida: " + entrada);
@@ -59,52 +61,5 @@ public class Principal {
 		}
 	}
 
-	@SuppressWarnings({ "unchecked", "rawtypes" })
-	public static void initTestData() throws Exception {
-		logger.info("Principal.initTestData: INICIO");
-		FileSystem fs = FileSystem.get(new Configuration());
-		fs.delete(new Path(Constants.DATA_ROOT), true);
-		fs.delete(new Path(Constants.OUTPUTS_ROOT), true);
-		fs.mkdirs(new Path(Constants.DATA_ROOT));
-		fs.mkdirs(new Path(Constants.OUTPUTS_ROOT + "edb"));
-
-		Pail<es.manning.schema.Data> masterPail = Pail.create(Constants.MASTER_ROOT, new SplitDataPailStructure());
-		Pail<es.manning.schema.Data> newPail = Pail.create(Constants.NEW_ROOT, new SplitDataPailStructure());
-
-		TypedRecordOutputStream os = masterPail.openWrite();
-
-		os.writeObject(es.manning.test.Data.makePageview(1, "http://foo.com/post1", 60));
-		os.writeObject(es.manning.test.Data.makePageview(3, "http://foo.com/post1", 62));
-		os.writeObject(es.manning.test.Data.makePageview(1, "http://foo.com/post1", 4000));
-		os.writeObject(es.manning.test.Data.makePageview(1, "http://foo.com/post2", 4000));
-		os.writeObject(es.manning.test.Data.makePageview(1, "http://foo.com/post2", 10000));
-		os.writeObject(es.manning.test.Data.makePageview(5, "http://foo.com/post3", 10600));
-		os.writeObject(es.manning.test.Data.makeEquiv(1, 3));
-		os.writeObject(es.manning.test.Data.makeEquiv(3, 5));
-
-		os.writeObject(es.manning.test.Data.makePageview(2, "http://foo.com/post1", 60));
-		os.writeObject(es.manning.test.Data.makePageview(2, "http://foo.com/post3", 62));
-		os.writeObject(es.manning.test.Data.makePersonPropertyValueFull_name(1, "Pepito"));
-		os.writeObject(es.manning.test.Data.makePersonPropertyValueFull_name(2, "Pepita"));
-		os.writeObject(es.manning.test.Data.makePersonPropertyValueGender(1, GenderType.MALE));
-		os.writeObject(es.manning.test.Data.makePersonPropertyValueGender(2, GenderType.FEMALE));
-		os.writeObject(es.manning.test.Data.makePersonPropertyValueLocation(1, "Miami", "FL", "USA"));
-		os.writeObject(es.manning.test.Data.makePersonPropertyValueLocation(2, "LA", "California", "USA"));
-		os.close();
-		
-		os = newPail.openWrite();		
-		os.writeObject(es.manning.test.Data.makePageview(7, "http://foo.com/post3", 60));
-		os.writeObject(es.manning.test.Data.makePageview(7, "http://foo.com/post1", 62));
-		os.writeObject(es.manning.test.Data.makePageview(9, "http://foo.com/post1", 60));
-		os.writeObject(es.manning.test.Data.makePageview(9, "http://foo.com/post2", 4000));
-		os.writeObject(es.manning.test.Data.makePersonPropertyValueFull_name(7, "Cata"));
-		os.writeObject(es.manning.test.Data.makePersonPropertyValueGender(7, GenderType.FEMALE));
-		os.writeObject(es.manning.test.Data.makePersonPropertyValueLocation(7, "Madrid", "MAD", "Spain"));
-		os.writeObject(es.manning.test.Data.makePersonPropertyValueFull_name(9, "Mario"));
-		os.writeObject(es.manning.test.Data.makePersonPropertyValueGender(9, GenderType.FEMALE));
-		os.writeObject(es.manning.test.Data.makePersonPropertyValueLocation(9, "Madrid", "MAD", "Spain"));
-		os.close();
-		logger.info("Principal.initTestData: FIN");
-	}
-
+	
 }
